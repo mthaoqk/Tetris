851c3b07f68ea18b5443b496fbc902e379b9e00a
Mthao <mthaoqk@users.noreply.github.com>
2019-03-11 18:42:47 -0500

    Temporary fixed controls

--- assets/JavaScript/tetris.js
+++ assets/JavaScript/tetris.js
@@ -90,7 +90,7 @@ Piece.prototype.draw = function () {
     this.fill(this.color);
 }
 
-//Undraw piece so when it goes down its is not contiously adding
+//Undraw piece so when it goes down its is not continuously adding
 
 Piece.prototype.unDraw = function () {
     this.fill(VACANT);
@@ -252,17 +252,21 @@ document.addEventListener("keydown", CONTROL);
 
 function CONTROL(event) {
     if (event.keyCode == 37) {
+        event.preventDefault()
         p.moveLeft();
         dropStart = Date.now();
     }
     else if (event.keyCode == 38) {
+        event.preventDefault()
         p.rotate();
     }
     else if (event.keyCode == 39) {
+        event.preventDefault()
         p.moveRight();
         dropStart = Date.now();
     }
     else if (event.keyCode == 40) {
+        event.preventDefault()
         p.moveDown(0);
     }
 }
